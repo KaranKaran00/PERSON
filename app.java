5cfe4c98ec0087b1dc257b139824447f6112a38a
KaranKaran00 <kharakaran504@gmail.com>
2026-02-28 23:54:28 +0530

    add background img

---
+++ app.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class app {
+    public static void main(String[] args) {
+        int boardWidth = 500;
+        int boardHeight = 500;
+
+        JFrame frame = new JFrame("PERSON");
+
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        person Person = new person();
+        frame.add(Person);
+        frame.pack();
+        frame.requestFocus();
+
+        frame.setVisible(true);
+    }
+}
---
+++ person.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class person extends JPanel {
+    int boardWidth = 500;
+    int boardHeight = 500;
+
+    // image
+    Image backgroundImg;
+
+    person() {
+
+    }
+}
