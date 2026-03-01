7fee52f12b8e437ef096d89cb8b1807e4218824a
KaranKaran00 <kharakaran504@gmail.com>
2026-03-01 12:34:39 +0530

    change

--- app.java
+++ app.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import javax.swing.*;
 
 public class app {
@@ -15,26 +14,8 @@ public class app {
 
         person Person = new person();
         frame.add(Person);
-        frame.pack();
+
         frame.requestFocus();
-
         frame.setVisible(true);
     }
-=======
-import javax.swing.*;
-
-public class app {
-    public static void main(String[] args) {
-        int boardWidth = 500;
-        int boardHeight = 500;
-
-        JFrame frame = new JFrame("PERSON");
-
-        frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
->>>>>>> 3dde41b9378a022b0a9262c392da3a28f58f9760
 }
