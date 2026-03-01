5d3b32b7ef6ca79930d6a84cccf536a0a7809f73
KaranKaran00 <kharakaran504@gmail.com>
2026-03-01 22:52:16 +0530

    backimg

--- person.java
+++ person.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.*;
 
 public class person extends JPanel {
@@ -7,15 +10,23 @@ public class person extends JPanel {
     int boardHeight = 500;
 
     Image backgroundImg;
+    Image leftImg;
+    Image rightImg;
 
     person() {
-        backgroundImg = new ImageIcon(getClass().getResource("backgroundImg.png")).getImage();
         setPreferredSize(new Dimension(boardWidth, boardHeight));
+        //setBackground(Color.blue);
+        backgroundImg = new ImageIcon(getClass().getResource("backgroundImg.png")).getImage();
+        // leftImg = new ImageIcon(getClass().getResource("leftImg")).getImage();
+        // rightImg = new ImageIcon(getClass().getResource("rightImg")).getImage();
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g); // important!
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
     }
 }
