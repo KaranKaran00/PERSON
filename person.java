852f7fa72395a1abae3051a09cf1a51b2811c807
KaranKaran00 <kharakaran504@gmail.com>
2026-03-01 14:01:52 +0530

    backgroundimg

--- person.java
+++ person.java
@@ -1,17 +1,21 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Random;
 import javax.swing.*;
 
 public class person extends JPanel {
+
     int boardWidth = 500;
     int boardHeight = 500;
 
-    // image
     Image backgroundImg;
 
     person() {
+        backgroundImg = new ImageIcon(getClass().getResource("backgroundImg.png")).getImage();
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+    }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // important!
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
     }
 }
